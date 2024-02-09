73f9473742ca04228f25539e7a2d48edc760c6bb
Mauricio Villarreal <mauricio.villarreal@outlook.com.ar>
2024-02-09 16:22:59 -0300

    tp maurinho final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,22 +12,38 @@ export const Cart = () => {
     if (cantidadTotal === 0) {
         return (
             <>
-                <h2 className="carritoVacio">El carrito esta vacio</h2>
+                <div className="carritoVacio">
+                    <h3 >El carrito esta vacio</h3>
+                    <Link className="btnVerProducots" to="/">
+                        <div className="btnbg">
+                            <h6>VER PRODUCTOS</h6>
+
+                        </div>
+                    </Link>
+
+                </div>
             </>
         )
     }
     return (
-        <div className='Carrito'>
-            <p className="ruta">Inicio/ Carrito</p>
-            <h1>CARRITO</h1>
-            {
-                carrito.map(prod => <CartItem className="cartView" key={prod.id} {...prod} />)
-            }
-            <div>
-                <button className="btnChekout" onClick={() => vaciarCarrito()}> Vaciar Carrito </button>
+        <div>
+            <div className='Carrito'>
+                <p className="ruta">Inicio/ Carrito</p>
+                <h1>CARRITO</h1>
+                <div className="CarritoScroll">
+
+                {
+                    carrito.map(prod => <CartItem className="cartView" key={prod.id} {...prod} />)
+                }
+                </div>
             </div>
-            <div className="btnChekoutfc">
-                <Link className="link" to="/checkout"> <p>Finalizar Compra  -  Total:   ${total}  </p> </Link>
+            <div>
+                <div>
+                    <button className="btnChekout" onClick={() => vaciarCarrito()}> Vaciar Carrito </button>
+                </div>
+                <div className="btnChekoutfc">
+                    <Link className="link" to="/checkout"> <p>Finalizar Compra  -  Total:   ${total}  </p> </Link>
+                </div>
             </div>
         </div>
     )
