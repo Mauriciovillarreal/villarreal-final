ebb28b7387395b51cf54bc8178c4b14b0329686a
Mauricio Villarreal <mauricio.villarreal@outlook.com.ar>
2024-02-12 18:00:32 -0300

    tp maurinho final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -31,7 +31,7 @@ const ItemListContainer = () => {
   return (
     <>
       <div className='igBanner'></div>
-      <Container>
+      <Container fluid>
         <ItemList productos={productos} />
       </Container>
     </>
